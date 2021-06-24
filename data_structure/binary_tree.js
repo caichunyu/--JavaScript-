c52eecd69946504a165aa8b86c568a8eaedfd2f0
caichunyu <caichunyu0@gmail.com>
2021-06-24 22:44:50 +0800

    docs binary tree removeNode methods

--- data_structure/binary_tree.js
+++ data_structure/binary_tree.js
@@ -16,8 +16,8 @@ function BST() {
   this.root = null; //根节点
   this.insert = insert; //插入
   this.inOrder = inOrder; //中序
-  this.inOrder = preOrder(); //先序
-  this.inOrder = postOrder(); //后序
+  this.preOrder = preOrder; //先序
+  this.preOrder = postOrder; //后序
   this.getMin = getMin; //找最小节点
   this.getMax = getMax; //最大节点
   this.find = find; //给定值查找
@@ -51,7 +51,8 @@ function insert(data) {
 }
 
 //中序遍历，得到的数据是升序排列，先左下然后左下父节点，然后右，数据升序 用递归实现
-function inOrder(node) { //回忆下递归树，从上到底，然后到达递归边界，然后执行表达式向上
+//回忆下递归树，从上到底，然后到达递归边界，然后执行表达式向上
+function inOrder(node = this.root) { //默认参数
   if (node) {
     inOrder(node.left);
     console.log(node.show() + ' ');
@@ -87,7 +88,7 @@ nums.insert(3);
 nums.insert(99);
 nums.insert(22);
 console.log('inorder traversal:');
-inOrder(nums.root);
+nums.inOrder();
 console.log('------ preOrder');
 preOrder(nums.root);
 console.log('------ postOrder');
@@ -95,13 +96,13 @@ postOrder(nums.root);
 
 //查找的方法
 //查找最小值，二叉排序树性质可以得出，最小为最左侧节点
-function getMin() {
-  let current = this.root;
+function getMin(current = this.root) {
+  // let current = this.root;
   console.log(current, '111')
   while (current.left) {
     current = current.left;
   }
-  return current.data;
+  return current;
 }
 
 //查找最大值，二叉排序树性质可以得出，最小为最右侧节点
@@ -133,15 +134,19 @@ function find(data) {
 // 1 如果没有子节点，直接删除就行
 //2 如果只有一个子节点，原本指向他的要换位置
 //3 两个子节点，要么查找带删除节点左子树最大值，要么查右子树最小值
-function remove(data) { //参数为待删除数据
+function remove(data) {
   root = removeNode(this.root, data);
 }
-
-function removeNode(node, data) {
+//参数data为待删除数据，node默认是根节点
+//函数返回删除的节点，然后打印二叉树是删除后的二叉树
+//用chrome控制台看思路
+function removeNode(node, data ) {
   if (!node) { //空树
+    console.log('!node')
     return null;
   }
   if (data === node.data) {
+    console.log('data===nodedata')
     //没有子节点的节点
     if (!node.left && !node.right) {
       return null
@@ -155,15 +160,18 @@ function removeNode(node, data) {
       return node.left;
     }
     //两个节点的,本程序按照查右子树的最小值来
-    let tempNode = getMin(node.right.data); //找到右子树的最小节点
+    let tempNode = getMin(node.right); //找到右子树的最小节点
     node.data = tempNode.data;
     node.right = removeNode(node.right, tempNode.data); //向右赋值
+    console.log(' node.right1')
     return node;
   } else if (data < node.data) {
     node.left = removeNode(node.left, data);
+    console.log(' node.left')
     return node;
   } else { //向右下走类似查找那个
     node.right = removeNode(node.right, data);
+    console.log(' node.right2',node.right)
     return node;
   }
 }
@@ -171,7 +179,9 @@ function removeNode(node, data) {
 //测试
 console.log('------ getMin');
 // nums.getMin()
-console.log(nums.getMin())
+// console.log(nums.root)
+console.log(getMin(nums.root),nums.getMin())
 console.log(nums.getMax())
 console.log(nums.find(22), 'find',)
-console.log('------ getMin');
+console.log('------ remove');
+console.log(removeNode(nums.root,23))
