b5984c3594346ac1042e44fc4b9531d4db4558fc
caichunyu <caichunyu0@gmail.com>
2021-07-05 22:47:42 +0800

    docs sort selectionSort

--- data_structure/sort.js
+++ data_structure/sort.js
@@ -6,7 +6,7 @@ function bubbleSort(data) {
   let temp; //临时变量，用来存交换的值
   for (let outer = data.length; outer > 1; outer--) { //每冒泡一轮，最后一个为已排好序的，需要排序的总量减一，直到最后一个
     console.log(data, '每轮冒泡'); //这个是每轮冒泡的结果
-    for (let inner = 0; inner <= outer - 1; inner++) { //从前向后，到outer-1是到排好序的位置
+    for (let inner = 0; inner < outer; inner++) { //从前向后，到outer-1是到排好序的位置
       if (data[inner] > data[inner + 1]) { //比较大小，交换
         // console.log(data, '每次交换') //这个可以打印出每次交换的结果
         temp = data[inner];
@@ -23,19 +23,26 @@ let arry = [10, 8, 2, 4, 9, 5, 3];
 console.log('bubbleSort', bubbleSort(arry));
 
 //选择排序
-//第一轮：对除了第一个以外的元素进行排序，然后和第一个进行对比，然后交换，
-//第二轮：对第二个元素后面的元素排序，然后和第二个对比，然后交换，直到倒数第二个对比，交换完成，排序完成。
-function selectionSort(data) {
+//每一趟（i）在第i趟后面的待排序元素中选取最小的元素，作为有序数列的第i个元素，直到n-1趟排完
+function selectSort(data) {
+  // outer、min、inner都是代表指针的，temp;临时变量，用来存交换的值
   let min, temp;
-  for (let outer = 0; outer <= data.length - 2; outer++) {
+  for (let outer = 0; outer < data.length; outer++) {
+    console.log(data, '每轮选择排序'); //这个是每轮选择排序的结果
     min = outer;
-    for (let inner = outer + 1; inner<=data.length-1;inner++) {
-      if (data[inner]<data[min]){
-        min= inner;
+    for (let inner = outer + 1; inner < data.length; inner++) {
+      if (data[inner] < data[min]) { //当后面要排序的元素中的数据小于前面已排好顺序的元素时
+        min = inner;
+        //swap数据
+        temp = data[min];
+        data[min] = data[outer];
+        data[outer] = temp;
+        console.log(data, '每次选择排序'); //这个是每次选择排序的结果
       }
-      temp = data[inner];
-      data[inner] = data[inner + 1];
-      data[inner + 1] = temp;
+
     }
   }
 }
+
+let selectArry = [10, 8, 2, 4, 9, 5, 3];
+console.log('selectSort', selectSort(selectArry));
