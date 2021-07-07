1ef7c064a54685f59747f6045961a2a07bf755f5
caichunyu <caichunyu0@gmail.com>
2021-07-07 22:13:46 +0800

    docs sort insertSort

--- data_structure/sort.js
+++ data_structure/sort.js
@@ -23,7 +23,8 @@ let arry = [10, 8, 2, 4, 9, 5, 3];
 console.log('bubbleSort', bubbleSort(arry));
 
 //选择排序
-//每一趟（i）在第i趟后面的待排序元素中选取最小的元素，作为有序数列的第i个元素，直到n-1趟排完
+//将排序的序列分为两个部分，一部分是前面的有序部分，一部分是后面的无序部分，
+//每一轮交换（i）在第i轮后面的待排序元素中选取最小的元素，作为有序数列的第i个元素，直到n-1趟排完
 function selectSort(data) {
   // outer、min、inner都是代表指针的，temp;临时变量，用来存交换的值
   let min, temp; //我去掉了min，要是使用的话，要注意调换完元素数值后把min指针也交换
@@ -44,5 +45,32 @@ function selectSort(data) {
   }
 }
 
+//selectSort test
 let selectArry = [10, 8, 2, 4, 9, 5, 3];
 console.log('selectSort', selectSort(selectArry));
+
+//直接插入排序
+//将排序的序列分为两个部分，一部分是前面的有序部分，一部分是后面的无序部分，取后面的无序部分元素，在有序部分
+// 进行查找，有小于有序部分的就将元素后移添加进有序部分
+//
+function insertSort(data) {
+  let temp, inner; //
+  //默认第一个元素有序，然后整体的遍历其后所有元素，外面的循环是一趟，里面循环的是外面遍历元素里面一趟
+  for (let outer = 1; outer < data.length; outer++) {
+    temp = data[outer]; //让temp等于要排序的无序的元素数值
+    inner = outer-1; //inner是有序序列的最后一个元素指针，outer是无序的从前开始的第一个指针
+    console.log(data, '每轮直接插入排序'); //这个是每轮直接插入排序的结果
+    //inner>0是从后遍历一趟有序序列， data[inner] > temp是有序的比无序待排的大
+    while (inner > 0 && data[inner] > temp) {
+      data[inner] = data[inner - 1]
+      --inner;
+      console.log(data, '每次直接插入排序交换'); //这个是每次直接插入排序的结果
+    }
+    data[inner] = temp;
+  }
+}
+
+//insertSort test
+let insertArry = [10, 8, 2, 4, 9, 5, 3];
+console.log('insertSort', insertSort(insertArry));
+
