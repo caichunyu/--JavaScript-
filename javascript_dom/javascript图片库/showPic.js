281ca27cda2d8742ab3923994a8e61116c9411b0
caichunyu <caichunyu0@gmail.com>
2021-09-29 20:45:40 +0800

    docs JavaScript图片库 add addLoadEvent函数

--- javascript_dom/javascript图片库/showPic.js
+++ javascript_dom/javascript图片库/showPic.js
@@ -6,3 +6,17 @@ function showPic(whichpic) {
   let description = document.getElementById('description'); //获取p元素，让p内容变为a的title
   description.firstChild.nodeValue = text;
 }
+
+//把在页面加载完毕时候执行函数创建为一个队列，当这个处理函数上没有绑定函数时候，把新函数
+// 添加给他，绑定了的话就添加到现有的末尾。
+function addLoadEvent(func) {
+  let oldonload = window.onload;
+  if (typeof window.onload !== 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      oldonload();
+      func();
+    }
+  }
+}
